73e3c813c9b0c8bf741e0b9962d81b4bb498d625
Jay Thakkar <jaythakkar305@gmail.com>
2022-06-14 00:21:33 +0530

    list coins from table with pagination

--- app/Http/Controllers/CoinApiController.php
+++ app/Http/Controllers/CoinApiController.php
@@ -3,9 +3,16 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use App\Models\Coin;
 
 class CoinApiController extends Controller
 {
+    private $insertArray;
+    public function __construct()
+    {
+        $this->insertArray = [];
+    }
     /**
      * Display a listing of the coinapi.
      *
@@ -20,11 +27,29 @@ class CoinApiController extends Controller
             $response_array = json_decode($response, true);
             if ($request->getStatusCode() != '200') {
                 throw new \Exception(isset($response_array['message']) ? $response_array['message'] : 'Something went wrong while fetching data from api.');
-            }            
+            }
             $coin_data = json_decode($response);
+
+
+            $coinsCollection = collect($response_array);
+            $coinChunks = $coinsCollection->chunk(10);
+            $coinChunks->each(function ($coinchunk, $key) {
+                DB::beginTransaction();
+                $this->insertArray = [];
+                $coinchunk->each(function ($coin, $coinkey) {
+                    $coinCollection = collect($coin);
+                    $this->insertArray[] = [
+                        'asset_id' => $coinCollection->get('name'), 'exchange_id' => $coinCollection->get('exchange_id'), 'website' => $coinCollection->get('website'), 'name' => $coinCollection->get('name'), 'data_start' => $coinCollection->get('data_start'), 'data_end' => $coinCollection->get('data_end'), 'data_quote_start' => $coinCollection->get('data_quote_start'), 'data_quote_end' => $coinCollection->get('data_quote_end'), 'data_symbols_count' => $coinCollection->get('data_symbols_count'), 'volume_1hrs_usd' => $coinCollection->get('volume_1hrs_usd'), 'volume_1day_usd' => $coinCollection->get('volume_1day_usd'), 'volume_1mth_usd' => $coinCollection->get('volume_1mth_usd'), 'json_object' => $coinCollection->toJson()
+                    ];
+                });
+                if (count($this->insertArray) > 0) {
+                    Coin::upsert($this->insertArray, ['name','asset_id'], array_keys($this->insertArray[0]));
+                }
+                DB::commit();
+            });            
             return response()->json(['coindata' => $coin_data, 'message' => 'Succesfully fetched'], 200);
         } catch (\Exception $e) {
             return response()->json(['message' => $e->getMessage()], 201);
-        }        
+        }
     }
 }
--- app/Http/Controllers/CoinController.php
+++ app/Http/Controllers/CoinController.php
@@ -2,14 +2,13 @@
 
 namespace App\Http\Controllers;
 
-
+use App\Models\Coin;
 use Illuminate\Http\Request;
 
 class CoinController extends Controller
 {
     public function __construct()
     {
-        
     }
     /**
      * Display a listing of the coinapi.
@@ -19,19 +18,25 @@ class CoinController extends Controller
     public function index(Request $request1)
     {
         try {
-            $client = new \GuzzleHttp\Client();
-            $request = $client->get(url('/api/coinapi'));
-            $response = $request->getBody()->getContents();
-            
-            $response_array = json_decode($response, true);
-            if ($request->getStatusCode() != '200') {
-                throw new \Exception($response_array['message']);
-            }            
-            $coin_data = $response_array['coindata'];
-            //return view('coin', compact('coin_data'));
+
+            $coins = Coin::paginate(10);
+            return view('coin.index', compact('coins'));
+            // $showEmployeeData = DB::table('employees')->paginate(4); //Query Builder
+
+            // $showEmployeeData = Employee::paginate(4); //Eloquent ORM
+
+            // return view('Employee.view', compact('showEmployeeData'));
+            // $client = new \GuzzleHttp\Client();
+            // $request = $client->get(url('/api/coinapi'));
+            // $response = $request->getBody()->getContents();
+            // $response_array = json_decode($response, true);
+            // if ($request->getStatusCode() != '200') {
+            //     throw new \Exception($response_array['message']);
+            // }            
+            // $coin_data = $response_array['coindata'];
+            // return view('coin', compact('coin_data'));
         } catch (\Exception $e) {
-            dd($e->getMessage());
-            //return view('error', ['error' => 'Uh oh! ' . $e->getMessage()]);
+            // return view('error', ['error' => 'Uh oh! ' . $e->getMessage()]);
         }
     }
 }
